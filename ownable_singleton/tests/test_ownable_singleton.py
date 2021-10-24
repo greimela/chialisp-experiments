89f2406ff5dcbbec0a551a76b59fcf8557986d01
Andreas Greimel <andreas.greimel@tngtech.com>
2021-10-24 12:02:28 +0200

    Add some test assertions

--- ownable_singleton/tests/test_ownable_singleton.py
+++ ownable_singleton/tests/test_ownable_singleton.py
@@ -1,25 +1,21 @@
-import pytest
+from typing import List, Optional
 
-from typing import Dict, List, Optional
+import pytest
+from cdv.test import CoinWrapper
+from cdv.test import setup as setup_test
 
 from chia.consensus.default_constants import DEFAULT_CONSTANTS
-
-from chia.types.blockchain_format.program import Program
-
 from chia.types.blockchain_format.coin import Coin
+from chia.types.blockchain_format.program import Program
 from chia.types.spend_bundle import SpendBundle
-from chia.types.condition_opcodes import ConditionOpcode
 from chia.util.ints import uint64
 from chia.wallet.puzzles import singleton_top_layer
 from chia.wallet.sign_coin_spends import sign_coin_spends
-
 from ownable_singleton.drivers.ownable_singleton_driver import (
     create_ownable_singleton, create_inner_puzzle
 )
 
-from cdv.test import CoinWrapper
-from cdv.test import setup as setup_test
-
+SINGLETON_AMOUNT: uint64 = 1023
 
 class TestOwnableSingleton:
     @pytest.fixture(scope="function")
@@ -32,12 +28,10 @@ class TestOwnableSingleton:
     async def test_piggybank_contribution(self, setup):
         network, alice, bob = setup
         try:
-            CONTRIBUTION_AMOUNT = 1023
-
             await network.farm_block(farmer=alice)
 
             # This retrieves us a coin that is at least 500 mojos.
-            contribution_coin: Optional[CoinWrapper] = await alice.choose_coin(CONTRIBUTION_AMOUNT)
+            contribution_coin: Optional[CoinWrapper] = await alice.choose_coin(SINGLETON_AMOUNT)
 
             conditions, launcher_coinsol = create_ownable_singleton(contribution_coin, alice.pk_)
 
@@ -46,7 +40,7 @@ class TestOwnableSingleton:
                 contribution_coin,
                 pushtx=False,
                 custom_conditions=conditions,
-                remain=alice, amt=CONTRIBUTION_AMOUNT
+                remain=alice, amt=SINGLETON_AMOUNT
             )
 
             launcher_spend: SpendBundle = await sign_coin_spends(
@@ -64,19 +58,28 @@ class TestOwnableSingleton:
 
             assert "error" not in result
 
-            print(result['additions'])
-            print(result['removals'])
-
-            # Eve Spend
+            # Make sure there is a singleton owned by alice
             launcher_coin: Coin = singleton_top_layer.generate_launcher_coin(
                 contribution_coin,
-                CONTRIBUTION_AMOUNT,
+                SINGLETON_AMOUNT,
             )
             launcher_id = launcher_coin.name()
+            alice_singleton_puzzle = singleton_top_layer.puzzle_for_singleton(launcher_id,
+                                                                              create_inner_puzzle(alice.pk_))
+            filtered_result: List[Coin] = list(
+                filter(
+                    lambda addition: (addition.amount == SINGLETON_AMOUNT)
+                                     and (
+                                             addition.puzzle_hash == alice_singleton_puzzle.get_tree_hash()
+                                     ),
+                    result["additions"],
+                )
+            )
+            assert len(filtered_result) == 1
 
-            alice_singleton_puzzle = singleton_top_layer.puzzle_for_singleton(launcher_id, create_inner_puzzle(alice.pk_))
-            singleton_coin: Coin = next(x for x in result['additions'] if
-                                        x.amount == CONTRIBUTION_AMOUNT and x.puzzle_hash != launcher_coin.puzzle_hash)
+            # Eve Spend
+            singleton_coin: Coin = next(
+                x for x in result['additions'] if x.puzzle_hash == alice_singleton_puzzle.get_tree_hash())
 
             lineage_proof: LineageProof = singleton_top_layer.lineage_proof_for_coinsol(launcher_coinsol)  # noqa
 
@@ -92,19 +95,28 @@ class TestOwnableSingleton:
             owner_change_spend = await alice.spend_coin(
                 CoinWrapper.from_coin(singleton_coin, puzzle=alice_singleton_puzzle), pushtx=False, args=full_solution)
 
-            print(owner_change_spend.coin_spends)
             result = await network.push_tx(owner_change_spend)
 
             singleton_eve_coinsol = owner_change_spend.coin_spends[0]
 
             assert "error" not in result
 
-            print(result['additions'])
-            print(result['removals'])
+            # Make sure there is a singleton owned by bob
+            bob_singleton_puzzle = singleton_top_layer.puzzle_for_singleton(launcher_id, create_inner_puzzle(bob.pk_))
+            filtered_result: List[Coin] = list(
+                filter(
+                    lambda addition: (addition.amount == SINGLETON_AMOUNT)
+                                     and (
+                                             addition.puzzle_hash == bob_singleton_puzzle.get_tree_hash()
+                                     ),
+                    result["additions"],
+                )
+            )
+            assert len(filtered_result) == 1
 
             # POST-EVE
-            singleton_coin: Coin = next(x for x in result['additions'] if
-                                        x.amount == CONTRIBUTION_AMOUNT and x.puzzle_hash != launcher_coin.puzzle_hash)
+            singleton_coin: Coin = next(
+                x for x in result['additions'] if x.puzzle_hash == bob_singleton_puzzle.get_tree_hash())
 
             lineage_proof: LineageProof = singleton_top_layer.lineage_proof_for_coinsol(singleton_eve_coinsol)  # noqa
 
@@ -117,7 +129,6 @@ class TestOwnableSingleton:
                 inner_solution,
             )
 
-            bob_singleton_puzzle = singleton_top_layer.puzzle_for_singleton(launcher_id, create_inner_puzzle(bob.pk_))
             owner_change_spend = await bob.spend_coin(
                 CoinWrapper.from_coin(singleton_coin, puzzle=bob_singleton_puzzle), pushtx=False, args=full_solution)
 
@@ -125,20 +136,16 @@ class TestOwnableSingleton:
 
             assert "error" not in result
 
-            print(result['additions'])
-            print(result['removals'])
-            #
-            # # Make sure there is exactly one piggybank with the new amount
-            # filtered_result: List[Coin] = list(
-            #     filter(
-            #         lambda addition: (addition.amount == 501)
-            #         and (
-            #             addition.puzzle_hash == create_piggybank_puzzle(
-            #                 1000000000000, bob.puzzle_hash).get_tree_hash()
-            #         ),
-            #         result["additions"],
-            #     )
-            # )
-            # assert len(filtered_result) == 1
+            # Make sure there is a singleton owned by alice
+            filtered_result: List[Coin] = list(
+                filter(
+                    lambda addition: (addition.amount == SINGLETON_AMOUNT)
+                                     and (
+                                             addition.puzzle_hash == alice_singleton_puzzle.get_tree_hash()
+                                     ),
+                    result["additions"],
+                )
+            )
+            assert len(filtered_result) == 1
         finally:
             await network.close()
